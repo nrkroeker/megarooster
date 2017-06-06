bfba567a67fab9d75c394bf2f79af0a44975562b
nrkroeker <nathaliekroeker@gmail.com>
2017-06-06 15:25:27 -0400

    Add new chickens to list

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const chickenForm = document.querySelector('.newChicken')
-const chickens = []
+const chickenList = [{breed: 'Buckeye', country: 'United States'}, {breed: 'Norfolk Grey', country: 'United Kingdom'}, {breed: 'Chios Fighting Chicken', country: 'Greece'}]
+const listBox = document.querySelector('.chickenList')
 
+
+function renderListItem(chicken) {
+    const li = document.createElement('li')
+    li.innerHTML = `${chicken.breed}, ${chicken.country}`
+    listBox.appendChild(li)
+}
 
 function renderList() {
-  
+  for(var i = 0; i < chickenList.length; i++) {
+    renderListItem(chickenList[i])
+  }
 }
 
 function onSubmit(ev) {
   ev.preventDefault()
   const form = ev.target
-  const listBox = document.querySelector('.chickenList')
+
 
   const newChicken = {
     breed: form.chickenBreed.value,
     country: form.country.value,
   }
 
-  chickens.push(newChicken)
+  chickenList.push(newChicken)
 
-  listBox.appendChild(renderList())
+  renderListItem(newChicken)
 }
 
-chickenForm.addEventListener('submit', onSubmit)
+document.addEventListener("DOMContentLoaded", function(event) {
+  renderList()
+  chickenForm.addEventListener('submit', onSubmit)
+});
